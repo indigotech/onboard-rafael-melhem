88725178811be8730836a7ecd93fd23026ee6456
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-09 15:13:49 -0300

    Add CreateUser endpoint

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { CreateUser } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
+  controllers: [CreateUser],
 })
 export class UsersModule {}
