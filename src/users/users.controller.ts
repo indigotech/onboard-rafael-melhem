b3b8513db1e5412d74d3b3a5b9c190b87a62ecc1
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-10 08:43:02 -0300

    Setup database input validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,20 +3,38 @@ import { Request } from 'express';
 import { UsersService } from './users.service';
 
 @Controller('users')
-export class CreateUser {
+export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   async create(@Req() req: Request) {
-    const body = req.body;
-    const userData = {
-      name: body.name,
-      email: body.email,
-      password: body.password,
-      birthDate: body.birthDate,
-    };
-    const user = await this.usersService.create(userData);
-    const { password, ...userWithoutPassword } = user;
+    const { name, email, password, birthDate } = req.body;
+
+    if (!password || password.length < 6) {
+      return { error_message: 'Password should have at least 6 characters' };
+    }
+
+    if (!/[A-Za-z]/.test(password) || !/\d/.test(password)) {
+      return {
+        error_message:
+          'Password should have at least one digit/number and one letter.',
+      };
+    }
+
+    const existingUser = await this.usersService.findByEmail(email);
+    if (existingUser) {
+      return {
+        error_message: 'Account already created using this email.',
+      };
+    }
+
+    const user = await this.usersService.create({
+      name,
+      email,
+      password,
+      birthDate,
+    });
+    const { password: _, ...userWithoutPassword } = user;
     return userWithoutPassword;
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
-import { CreateUser } from './users.controller';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
-  controllers: [CreateUser],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
