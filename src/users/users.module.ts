9a445c13af8a1518eef1a8531d167ccfd8989157
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-10 08:43:02 -0300

    Setup database input validation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
-import { CreateUser } from 'src/users/users.controller';
+import { UsersController } from 'src/users/users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
-  controllers: [CreateUser],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule { }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -26,4 +26,8 @@ export class UsersService {
   async remove(id: number): Promise<void> {
     await this.usersRepository.delete(id);
   }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOneBy({ email });
+  }
 }
