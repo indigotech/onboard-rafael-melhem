76d6478d7ed4474f3a8346f0c313fe02688f7dd6
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-10 08:43:02 -0300

    Setup database input validation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
-import { UsersController, UserLoginController } from 'src/users/users.controller';
-import { UsersService } from './users.service';
+import {
+  UsersController,
+  UserLoginController,
+} from 'src/users/users.controller';
+import { UsersService } from 'src/users/users.service';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
