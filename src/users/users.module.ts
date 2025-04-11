32f4902aee6f983f8f44d82d211e11cac1696906
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-10 22:11:49 -0300

    feat: add login-endpoint

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
-import { UsersController } from './users.controller';
+import { UsersController, UserLoginController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
-  controllers: [UsersController],
+  controllers: [UsersController, UserLoginController],
   providers: [UsersService],
 })
 export class UsersModule {}
