9b0b840e4e0a220426a4dac7e7395e308e625f9b
Rafael Melhem <rafael.melhem@taqtile.com.br>
2025-04-11 21:40:09 -0300

    Set environment variables and sources

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users.entity';
-import { CreateUser } from './users.controller';
+import { User } from 'src/users/users.entity';
+import { CreateUser } from 'src/users/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
